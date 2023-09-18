05eacb1dd4bf7dc55370dd51e6baa5bae5aad16b
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-09-18 13:54:05 -0300

    implementation of the footer on the website

--- beyound/src/components/layout/Footer.js
+++ beyound/src/components/layout/Footer.js
@@ -1,6 +1,17 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa';
+
+import styles from './Footer.module.css';
+
 function Footer(){
     return(
-        <p>Footer</p>
+        <footer className={styles.footer}>
+            <ul className={styles.socialList}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p className={styles.copyRight }><span>Beyound</span> &copy; 2023</p>
+        </footer>
     )
 }
 
