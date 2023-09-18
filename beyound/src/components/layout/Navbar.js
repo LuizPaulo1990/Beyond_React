692685736755d5c259ec13aff02f9b0ac87d6677
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-09-18 15:03:58 -0300

    home  was created, home css, btn newprojects ,css

--- beyound/src/components/layout/Navbar.js
+++ beyound/src/components/layout/Navbar.js
@@ -8,26 +8,24 @@ import logo from '../../img/costs_logo.png';
 function Navbar() {
     return (
         <nav className={styles.navbar}>
-            <Container>
-                <Link to='/'>
-                    <img src={logo} alt='Beyound' />
-                </Link>
-                <ul className={styles.list}>
-                    <li className={styles.item}>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link to='/project'>Projects</Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link to='/company'>Company</Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
+            <Link to='/'>
+                <img src={logo} alt='Beyound' />
+            </Link>
+            <ul className={styles.list}>
+                <li className={styles.item}>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li className={styles.item}>
+                    <Link to='/project'>Projects</Link>
+                </li>
+                <li className={styles.item}>
+                    <Link to='/company'>Company</Link>
+                </li>
+                <li className={styles.item}>
+                    <Link to='/contact'>Contact</Link>
+                </li>
 
-                </ul>
-            </Container>
+            </ul>
         </nav>
     )
 }
--- beyound/src/components/pages/Home.js
+++ beyound/src/components/pages/Home.js
@@ -1,5 +1,16 @@
+import styles from './Home.module.css';
+import saving from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton';
+
 function Home() {
-    return <h1>Home</h1>
+    return(
+        <section className={styles.homeContainer}>
+            <h1>Welcome to <span>Beyound</span></h1>
+            <p>start managing you projects right now!</p>
+            <LinkButton to='/newproject' text='Create Project'/>
+            <img src={saving} alt='Beyound'></img>
+        </section>
+    )
 }
 
 export default Home;
